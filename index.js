83bd4a21680b0f17efa1e4e52cc2a49dff793958
Hope Barrow <barr.writing@gmail.com>
2022-07-21 17:56:39 -0500

    longest index file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const generateHTML = require(`./Develop/util/generateHtml`);
 
 //arrays for storage
 function userInfo() {
-    inquirer.prompt([
+    inquirer.prompt ([
         type: "input",
         message: "Please enter the employee name here.",
         name: "names",
@@ -26,22 +26,74 @@ function userInfo() {
                 return "Please try again, this time with the employee's name.";
             }
         }
-        },
-{
+    },
+    {
         type: "input",
-        message: "enter email here",
+        message: "Please enter the employee's email here",
         name: "email",
-        function (nameInput) {
-            if (nameInput) {
+        function (emailInput) {
+            if (emailInput) {
                 return true;
             } else {
-                return "Please try again, this time with the employee's name.";
+                return "I'm sorry, please re-enter the employee's email.";
             }
         }
-            
-        
-
-
-
+    },
+    {
+        type: "input",
+        message: "Please enter the employee's ID here",
+        name: "ID",
+        function (idInput) {
+            if (idInput) {
+                return true;
+            } else {
+                return "I'm sorry, please re-enter the employee's ID."
+            }
+        }
+    },
+    {
+        type: "list",
+        message: "Please select the role of the employee."
+        name: "role"
+        choices: ["Manager, Engineer, Intern,"]
+        }
     ])
-}
+        .then(answers => {
+            if (answers.role === 'Manager') {
+                type: "input",
+                message: "Please enter the corresponding office number.",
+                name: "office,"
+                (officeInput) => {
+                    if (officeInput) {
+                        return true;
+                        } else {
+                            return "Please enter office number.";
+                        }
+                    }
+                }
+            ])
+
+            .then(response => {
+                console.log(response.office);
+                const ManagerTeam = newManager (answers.name, answers.email, answers.id, answers.role, response.office);
+                teamMembers.push(ManagerTeam);
+                addOption()
+            })
+
+        } else if (answers.role === 'Engineer'){
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter GitHub username",
+                    name: "GitHub",
+                    githubInput => {
+                        if (githubInput) {
+                            return true; 
+                        } else {
+                            return "Please enter GitHub username";
+                        }
+                        }
+                    }
+                }
+            ])
+        
